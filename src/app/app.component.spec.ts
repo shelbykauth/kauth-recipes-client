f02bad84b61ce38f5b7d9b7f4fc88d36ecdaf5fb
Shelby Kauth <shelbykauth@gmail.com>
2020-12-19 01:11:08 -0500

    Remove Silly Title Test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,12 +22,12 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('recipe-sharing-client');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain(
-      'recipe-sharing-client app is running!'
-    );
-  });
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement;
+  //   expect(compiled.querySelector('.content span').textContent).toContain(
+  //     'recipe-sharing-client app is running!'
+  //   );
+  // });
 });
