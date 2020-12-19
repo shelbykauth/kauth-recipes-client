d652b332d243a18c74b669a564b18521cc0372b6
Shelby Kauth <shelbykauth@gmail.com>
2020-12-19 00:47:53 -0500

    Separate Side Menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/views/main/main.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/side-menu/side-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'recipes-side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrls: ['./side-menu.component.less']
+})
+export class SideMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
