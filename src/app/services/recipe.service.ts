adb66a32c58ca4c052d651bb683dedf43e640442
Shelby Kauth <shelbykauth@gmail.com>
2020-12-20 01:43:19 -0500

    Add RecipeService.findBySlug

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, from, Observable } from 'rxjs';
 import { take, map, tap } from 'rxjs/operators';
 import { Recipe, RecipeFactory } from './types';
 
@@ -11,9 +11,18 @@ export class RecipeService {
   private _allItems = new BehaviorSubject<Recipe[]>([]);
   public allItems = this._allItems.asObservable();
 
+  private _itemsById: { [x: string]: Recipe } = {};
+  private addItemPipe = map((recipe: Recipe) => {
+    this._itemsById[recipe.id] = recipe;
+    this._itemsById[recipe.slug] = recipe;
+  });
+
   constructor(private http: HttpClient) {
     this.loadAllRecipes();
-    this.allItems.pipe(tap((data) => console.log(data))).subscribe();
+    this.findBySlug('cake').subscribe((recipes) => console.log(recipes));
+    this.allItems
+      .pipe(tap((data) => console.log(data, this._itemsById)))
+      .subscribe();
   }
 
   loadAllRecipes() {
@@ -23,8 +32,30 @@ export class RecipeService {
         take(1),
         map((data) => data as Recipe[]),
         map((data) => data.map((recipe) => RecipeFactory(recipe))),
+        tap((recipes) => from(recipes).pipe(this.addItemPipe).subscribe()),
         tap((recipes) => this._allItems.next(recipes))
       )
       .subscribe();
+  }
+
+  findBySlug(identifier: string): Observable<Recipe[]> {
+    identifier = identifier.toLowerCase();
+    return this.allItems.pipe(
+      map((allRecipes) => {
+        if (this._itemsById[identifier]) {
+          return [this._itemsById[identifier]];
+        }
+        let possibleRecipes = allRecipes.filter((recipe) => {
+          if (recipe.slug.includes(identifier)) {
+            return true;
+          }
+          if (recipe.id.includes(identifier)) {
+            return true;
+          }
+          return false;
+        });
+        return possibleRecipes;
+      })
+    );
   }
 }
